cf883740adc159d6187a06ff18b2b873c5ae1743
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2023-09-02 20:30:24 +0530

    merch page done

--- src/Pages/Merch.jsx
+++ src/Pages/Merch.jsx
@@ -1,15 +1,44 @@
 import React from 'react'
-import { Nav } from '../Components/Nav'
-import { Footer } from '../Components/Footer'
-import { Shop } from '../Components/Shop'
+// import { Nav } from '../Components/Nav'
+// import { Footer } from '../Components/Footer'
+// import { Shop } from '../Components/Shop'
+import merch1 from "../assets/images/1.png"
+import merch2 from "../assets/images/2.png"
+import merch3 from "../assets/images/3.png"
+// import Slider from '../Components/Slider'
 
 export const Merch = () => {
     return (
         <>
-            <div>Merch</div>
-            <Nav />
-            <Shop />
-            <Footer />
+            <div className='bg-gradient-to-r from-yellow-400 to-[#F22223] h-full pt-32 pb-10 flex items-center justify-center md:flex-col text-white sm:flex-col'>
+                <div className='w-fit h-fit bg-gray-500 flex items-center justify-center mx-28 rounded-xl shadow-2xl gap-48'>
+                    <div className='flex items-center justify-center ml-28 my-32 border-4 border-black'>
+                        <div>
+
+                            <img src={merch1} alt="" className='w-[800px] hover:scale-150 duration-500 hover:cursor-pointer hover:shadow-2xl' />
+                        </div>
+                        <div>
+                            <img src={merch2} alt="" className='w-[400px] hover:scale-[2.5] duration-500 hover:cursor-pointer hover:shadow-2xl' />
+                            <img src={merch3} alt="" className='w-[400px] hover:scale-[2.5] duration-500 hover:cursor-pointer hover:shadow-2xl' />
+                        </div>
+                    </div>
+                    <div className=''>
+                        <div className='w-[70%] flex flex-col items-start justify-center'>
+                            <h3 className='text-4xl py-6 font-bold'>Roots Merchandise T-Shirt</h3>
+                            <p className=''>Get ready to make a statement with our new t-shirt ! Made with high-quality, soft material, this shirt is perfect for any occasion. Whether you're hanging out with friends or running errands, this shirt will keep you comfortable and stylish. Plus, with its eye-catching design, you'll be turning heads wherever you go. Don't miss out on this must-have addition to your wardrobe. Order yours today!</p>
+                        </div>
+                        <div className='flex gap-36 pt-8 items-center justify-start'>
+                            <div>
+                                <p className='font-extrabold text-3xl'>₹385</p>
+                            </div>
+                            <div>
+                                <button className='border-2 border-black p-2 rounded-3xl hover:bg-black hover:text-white hover:shadow-2xl duration-300'>Buy Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
--- src/Pages/Team.jsx
+++ src/Pages/Team.jsx
@@ -135,7 +135,7 @@ const Team = () => {
             title: "Jagyaraj Pattnaik",
             desg: "Travel & Logistic Team",
             src: tech1,
-        },  
+        },
         {
             id: 2,
             title: "Soumyakanta Prusty",
@@ -193,175 +193,177 @@ const Team = () => {
     ]
 
     return (
-        <div className='w-full h-full bg-black pt-16 flex flex-col justify-center items-center pb-10'>
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%]'>
-                <div className='text-5xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223]'>Our Patron</div>
+        <>
+            <div className='w-full h-full bg-black pt-16 flex flex-col justify-center items-center pb-10 -z-50'>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] -z-0'>
+                    <div className='text-5xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223]'>Our Patron</div>
 
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-12 px-24'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center'>
-                        <div className="profile-card">
-                            <div className="img">
-                                <img src={director} alt="" />
-                            </div>
-                            <div className="caption">
-                                <h3>K. Umamaheswar Rao</h3>
-                                <p>Director</p>
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-12 px-24'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center'>
+                            <div className="profile-card">
+                                <div className="img">
+                                    <img src={director} alt="" />
+                                </div>
+                                <div className="caption">
+                                    <h3>K. Umamaheswar Rao</h3>
+                                    <p>Director</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%]'>
-                <div className='text-5xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223]'>SAC Officials</div>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%]'>
+                    <div className='text-5xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223]'>SAC Officials</div>
 
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-12'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        <div className="profile-card">
-                            <div className="img">
-                                <img src={dean} alt="" />
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-12'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            <div className="profile-card">
+                                <div className="img">
+                                    <img src={dean} alt="" />
+                                </div>
+                                <div className="caption">
+                                    <h3>Sidharth S. Jena</h3>
+                                    <p>Dean (SW)</p>
+                                </div>
                             </div>
-                            <div className="caption">
-                                <h3>Sidharth S. Jena</h3>
-                                <p>Dean (SW)</p>
+                            <div className="profile-card">
+                                <div className="img">
+                                    <img src={sacp} alt="" />
+                                </div>
+                                <div className="caption">
+                                    <h3>Poonam Singh</h3>
+                                    <p>SAC President</p>
+                                </div>
                             </div>
                         </div>
-                        <div className="profile-card">
-                            <div className="img">
-                                <img src={sacp} alt="" />
-                            </div>
-                            <div className="caption">
-                                <h3>Poonam Singh</h3>
-                                <p>SAC President</p>
-                            </div>
+                    </div>
+                </div>
+
+                <div className='text-7xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] mt-10'>Core Team</div>
+
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
+                    <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Technical Team</div>
+
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            {tech.map(({ id, title, desg, src }) => (
+                                <div className="profile-card" key={id}>
+                                    <div className="img">
+                                        <img src={src} alt="" />
+                                    </div>
+                                    <div className="caption">
+                                        <h3>{title}</h3>
+                                        <p>{desg}</p>
+                                    </div>
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='text-7xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] mt-10'>Core Team</div>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
+                    <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Event Management Team</div>
 
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
-                <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Technical Team</div>
-
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        {tech.map(({ id, title, desg, src }) => (
-                            <div className="profile-card" key={id}>
-                                <div className="img">
-                                    <img src={src} alt="" />
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            {event.map(({ id, title, desg, src }) => (
+                                <div className="profile-card" key={id}>
+                                    <div className="img">
+                                        <img src={src} alt="" />
+                                    </div>
+                                    <div className="caption">
+                                        <h3>{title}</h3>
+                                        <p>{desg}</p>
+                                    </div>
                                 </div>
-                                <div className="caption">
-                                    <h3>{title}</h3>
-                                    <p>{desg}</p>
-                                </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
-                <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Event Management Team</div>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
+                    <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Design & Video Editing Team</div>
 
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        {event.map(({ id, title, desg, src }) => (
-                            <div className="profile-card" key={id}>
-                                <div className="img">
-                                    <img src={src} alt="" />
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            {design.map(({ id, title, desg, src }) => (
+                                <div className="profile-card" key={id}>
+                                    <div className="img">
+                                        <img src={src} alt="" />
+                                    </div>
+                                    <div className="caption">
+                                        <h3>{title}</h3>
+                                        <p>{desg}</p>
+                                    </div>
                                 </div>
-                                <div className="caption">
-                                    <h3>{title}</h3>
-                                    <p>{desg}</p>
-                                </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
-                <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Design & Video Editing Team</div>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
+                    <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Travel & Logistics Team</div>
 
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        {design.map(({ id, title, desg, src }) => (
-                            <div className="profile-card" key={id}>
-                                <div className="img">
-                                    <img src={src} alt="" />
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            {travel.map(({ id, title, desg, src }) => (
+                                <div className="profile-card" key={id}>
+                                    <div className="img">
+                                        <img src={src} alt="" />
+                                    </div>
+                                    <div className="caption">
+                                        <h3>{title}</h3>
+                                        <p>{desg}</p>
+                                    </div>
                                 </div>
-                                <div className="caption">
-                                    <h3>{title}</h3>
-                                    <p>{desg}</p>
-                                </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
-                <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Travel & Logistics Team</div>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
+                    <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>PR Team</div>
 
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        {travel.map(({ id, title, desg, src }) => (
-                            <div className="profile-card" key={id}>
-                                <div className="img">
-                                    <img src={src} alt="" />
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            {pr.map(({ id, title, desg, src }) => (
+                                <div className="profile-card" key={id}>
+                                    <div className="img">
+                                        <img src={src} alt="" />
+                                    </div>
+                                    <div className="caption">
+                                        <h3>{title}</h3>
+                                        <p>{desg}</p>
+                                    </div>
                                 </div>
-                                <div className="caption">
-                                    <h3>{title}</h3>
-                                    <p>{desg}</p>
-                                </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
-                <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>PR Team</div>
+                <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
+                    <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Guest Hospitality & Workshop Team</div>
 
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        {pr.map(({ id, title, desg, src }) => (
-                            <div className="profile-card" key={id}>
-                                <div className="img">
-                                    <img src={src} alt="" />
+                    <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
+                        <div className='flex items-center flex-wrap gap-8 justify-center pt'>
+                            {guest.map(({ id, title, desg, src }) => (
+                                <div className="profile-card" key={id}>
+                                    <div className="img">
+                                        <img src={src} alt="" />
+                                    </div>
+                                    <div className="caption">
+                                        <h3>{title}</h3>
+                                        <p>{desg}</p>
+                                    </div>
                                 </div>
-                                <div className="caption">
-                                    <h3>{title}</h3>
-                                    <p>{desg}</p>
-                                </div>
-                            </div>
-                        ))}
+                            ))}
+                        </div>
                     </div>
                 </div>
+
             </div>
-
-            <div className='flex flex-col items-center before:blur-md mt-8 w-[85%] justify-center'>
-                <div className='text-4xl flex items-center justify-center font-bold text-transparent bg-clip-text bg-gradient-to-r from-yellow-300 to-[#F22223] pb-6'>Guest Hospitality & Workshop Team</div>
-
-                <div className='bg-gray-400 w-fit flex items-center justify-center h-fit rounded-xl mt-6 py-14'>
-                    <div className='flex items-center flex-wrap gap-8 justify-center pt'>
-                        {guest.map(({ id, title, desg, src }) => (
-                            <div className="profile-card" key={id}>
-                                <div className="img">
-                                    <img src={src} alt="" />
-                                </div>
-                                <div className="caption">
-                                    <h3>{title}</h3>
-                                    <p>{desg}</p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-
-        </div>
+        </>
     )
 }
 
